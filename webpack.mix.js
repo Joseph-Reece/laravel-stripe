9903f0d874c4f09c5734a51f931330d4d2a76a5d
joseph-reece <ndirashjose@gmail.com>
2022-04-27 15:17:20 +0300

    Add react and Auth

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,18 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
+    .react()
     .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+        require('postcss-import'),
+        require('tailwindcss'),
+        require('autoprefixer'),
+    ])
+    .alias({
+        '@': 'resources/js',
+    });
+
+if (mix.inProduction()) {
+    mix.version();
+}
+
+mix.browserSync('localhost:8000');
